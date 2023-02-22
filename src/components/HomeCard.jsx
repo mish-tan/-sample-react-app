530881f9d38c94a3d60885bda8e4fd7919c61fee
Michelle T <mish.mash.tan@gmail.com>
2023-02-22 11:48:49 +0000

    Add images and list

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -1,57 +1,49 @@
 import { useState } from 'react'
 import Button from '@mui/material/Button'
+import Grid from '@mui/material/Grid'
 import Card from '@mui/material/Card'
 import Divider from '@mui/material/Divider'
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
-import reactLogo from '../assets/react.svg'
+import Conan from '../assets/Conan.jpg'
+import Suzume from '../assets/Suzume.jpg'
+import ConanHalloween from '../assets/ConanHalloween.jpg'
+import Kuroko from '../assets/Kuroko.jpg'
 import './HomeCard.css'
 
 export default function HomeCard() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <Card>
+      <Grid></Grid>
       <Stack
         spacing={2}
         justifyContent="center"
         alignItems="center"
         paddingY={4}
       >
-        <Stack direction="row">
-          <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-            <img src="/sample-react-app/vite.svg" className="logo" alt="Vite logo" />
+        <Typography variant="h4">Movies I watched recently</Typography>
+        <Stack direction="column">
+          <a href="https://www.conan-movie.jp" target="_blank" rel="noreferrer">
+            <img src={Conan} height={250} alt="Conan" />
           </a>
-          <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-            <img src={reactLogo} className="logo react" alt="React logo" />
+          8/10
+          <a href="https://suzume-tojimari-movie.jp" target="_blank" rel="noreferrer">
+            <img src={Suzume} height={250} alt="Suzume" />
           </a>
+          6/10
+          <a href="https://www.conan-movie.jp" target="_blank" rel="noreferrer">
+            <img src={ConanHalloween} height={250} alt="ConanHalloween" />
+          </a>
+          9/10
+          <a href="http://kurobas-lg.com" target="_blank" rel="noreferrer">
+            <img src={Kuroko} height={250} alt="Kuroko" />
+          </a>
+          11/10
         </Stack>
-        <Typography variant="h1">Vite + React</Typography>
-        <Button
-          variant="contained"
-          sx={{ width: '150px' }}
-          onClick={() => setCount((counter) => counter + 1)}
-        >
-          count is
-          {' '}
-          {count}
-        </Button>
-
-        <Typography variant="body1">
-          Edit
-          {' '}
-          <code>src/App.jsx</code>
-          {' '}
-          and save to test HMR
-        </Typography>
-
-        <Typography variant="body2">
-          Click on the Vite and React logos to learn more
-        </Typography>
-
         <Divider />
-
       </Stack>
     </Card>
-  )
+  );
 }
